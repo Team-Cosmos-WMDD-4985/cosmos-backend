c92ed3bbe6350ddab117f79281704f93e5736135
AkashBawa <bawa2027@gmail.com>
2024-03-06 15:52:09 -0800

    [front-en]: intergration completed

--- server.js
+++ server.js
@@ -14,11 +14,13 @@ const app = express();
 // await awsService.getObject();
 
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({
-    extended: true,
-    limit:'50mb'
-}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
 app.use((req, res, next) => {
     console.log("params" , req.params);
     console.log("body" , req.body);
@@ -36,7 +38,7 @@ app.use ((err, req, res, next) => {
         err : err
     });
 })
-app.use(bodyParser.json());
+
 // app.use(authRoute)
 
 

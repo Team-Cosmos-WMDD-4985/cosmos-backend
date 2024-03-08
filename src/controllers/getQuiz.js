74056f781eeda6c703ae03e8a633d19d58a7c50d
Arash <76617498+arashghezavati@users.noreply.github.com>
2024-03-07 21:03:09 -0800

    getQuiz add

--- src/controllers/getQuiz.js
+++ src/controllers/getQuiz.js
@@ -1,14 +1,23 @@
-import Quiz from '../models/quizSchema.js';
-import authTokenRequired from '../middleware/authTokenRequired.js';
+import Quiz from "../models/Quiz.js";
+import authTokenRequired from "../middleware/authTokenRequired.js";
 
 export const getQuiz = async (req, res) => {
-    const courseId = req.params.courseId; 
+  const courseId = req.params.courseId;
 
-    try {
-        const quizzes = await Quiz.find({ courseId: courseId }).populate('questions');
-        res.json(quizzes); 
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ error: 'Internal Server Error' });
-    }
+  try {
+    const quizzes = await Quiz.find({ courseId: courseId }).populate(
+      "questions"
+    );
+    console.log(quizzes);
+    res.json(quizzes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+};
+
+export const sendTopics = async (req, res) => {
+  const courseId = req.body.courseId;
+  console.log(courseId);
+  console.log(req.body);
 };
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,25 +1,32 @@
-import express from 'express';
-import multer, {memoryStorage} from "multer";
-import authRoute from './authRoutes.js';
+import express from "express";
+import multer, { memoryStorage } from "multer";
+import authRoute from "./authRoutes.js";
 import * as TopicGenerationController from "../controllers/topicGeneration.js";
 import multerConfig from "./../services/multer.js";
 import AuthMiddleware from "./../middleware/authTokenRequired.js";
+import * as QuizController from "./../controllers/getQuiz.js";
 
 const router = express.Router();
 const upload = multer({ storage: multerConfig.multerConfig() });
 
-router.get("/test", (req, res)=> {
-    res.json({
-        message: "This is a test route"
-    })
+router.get("/test", (req, res) => {
+  res.json({
+    message: "This is a test route",
+  });
 });
 
 router.get("/gets3", TopicGenerationController.topicGeneration);
-router.get("/courses", AuthMiddleware, TopicGenerationController.getCourses )
-router.post("/addCourse",AuthMiddleware, upload.single('file'), TopicGenerationController.addCourse);
+router.get("/courses", AuthMiddleware, TopicGenerationController.getCourses);
+router.post(
+  "/addCourse",
+  AuthMiddleware,
+  upload.single("file"),
+  TopicGenerationController.addCourse
+);
 router.post("/generateTopics", TopicGenerationController.topicGeneration);
-router.post("/generateQuiz", TopicGenerationController.QuizGeneration )
-router.use("/auth" ,authRoute);
+router.post("/generateQuiz", TopicGenerationController.QuizGeneration);
+router.use("/auth", authRoute);
 
+router.post("/sendTopics", AuthMiddleware, QuizController.sendTopics);
 
 export default router;

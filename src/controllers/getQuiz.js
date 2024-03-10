d9d723dbd6be3548a28d64d9ce78161a6dd9cfea
Somchai Sik <103734177+aman7691@users.noreply.github.com>
2024-03-10 16:01:05 -0700

    Quiz Backend dynamic and front implement started
    * Login Signup backend completed
    
    * quiz saved to db
    
    * getQuiz add
    
    * merge
    
    * generating quiz get topics
    
    ---------
    
    Co-authored-by: Arash <76617498+arashghezavati@users.noreply.github.com>

--- src/controllers/getQuiz.js
+++ src/controllers/getQuiz.js
@@ -1,14 +1,32 @@
-import Quiz from '../models/quizSchema.js';
-import authTokenRequired from '../middleware/authTokenRequired.js';
+import Quiz from "../models/Quiz.js";
+import authTokenRequired from "../middleware/authTokenRequired.js";
+import generateQuizQuestion from "../services/openai-test.js";
 
 export const getQuiz = async (req, res) => {
-    const courseId = req.params.courseId; 
+  const courseId = req.params.courseId;
 
-    try {
-        const quizzes = await Quiz.find({ courseId: courseId }).populate('questions');
-        res.json(quizzes); 
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ error: 'Internal Server Error' });
-    }
+  try {
+    const quizzes = await Quiz.find({ courseId: courseId }).populate(
+      "questions"
+    );
+    console.log(quizzes);
+    res.json(quizzes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+};
+
+export const sendTopics = async (req, res) => {
+  const { courseId, topics, name, type, difficulty, numQuestions } = req.body;
+ console.log(courseId, topics, name, type, difficulty, numQuestions)
+
+  try {
+    const topicsString = topics.join(", ");
+    const quizTopic = await generateQuizQuestion(topicsString, courseId, name, type, difficulty, numQuestions)
+    console.log(topicsString)
+  } catch(err){
+    console.error("Error in sendTopics:", err);
+    res.status(500).send({ error: 'Failed to generate quiz' });
+  }
 };
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,25 +1,35 @@
-import express from 'express';
-import multer, {memoryStorage} from "multer";
-import authRoute from './authRoutes.js';
+import express from "express";
+import multer, { memoryStorage } from "multer";
+import authRoute from "./authRoutes.js";
 import * as TopicGenerationController from "../controllers/topicGeneration.js";
+import * as getQuiz from "../controllers/generateQuiz.js";
 import multerConfig from "./../services/multer.js";
 import AuthMiddleware from "./../middleware/authTokenRequired.js";
+import * as QuizController from "./../controllers/getQuiz.js";
+import * as generateQuiz from "../controllers/generateQuiz.js"
 
 const router = express.Router();
 const upload = multer({ storage: multerConfig.multerConfig() });
 
-router.get("/test", (req, res)=> {
-    res.json({
-        message: "This is a test route"
-    })
+router.get("/test", (req, res) => {
+  res.json({
+    message: "This is a test route",
+  });
 });
 
 router.get("/gets3", TopicGenerationController.topicGeneration);
-router.get("/courses", AuthMiddleware, TopicGenerationController.getCourses )
-router.post("/addCourse",AuthMiddleware, upload.single('file'), TopicGenerationController.addCourse);
+router.get("/courses", AuthMiddleware, TopicGenerationController.getCourses);
+router.post(
+  "/addCourse",
+  AuthMiddleware,
+  upload.single("file"),
+  TopicGenerationController.addCourse
+);
 router.post("/generateTopics", TopicGenerationController.topicGeneration);
 router.post("/generateQuiz", TopicGenerationController.QuizGeneration )
+router.get("/getQuiz", generateQuiz.getQuiz )
 router.use("/auth" ,authRoute);
 
+router.post("/sendTopics", AuthMiddleware, QuizController.sendTopics);
 
 export default router;

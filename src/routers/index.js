0b3d4de752e093bc5c4da983fba69a236b37d622
Akash Bawa <40368346+AkashBawa@users.noreply.github.com>
2024-03-10 21:10:35 -0700

    [Quiz]: fetch all quiz by user (#17)

--- src/routers/index.js
+++ src/routers/index.js
@@ -17,19 +17,20 @@ router.get("/test", (req, res) => {
   });
 });
 
-router.get("/gets3", TopicGenerationController.topicGeneration);
-router.get("/courses", AuthMiddleware, TopicGenerationController.getCourses);
-router.post(
-  "/addCourse",
-  AuthMiddleware,
-  upload.single("file"),
-  TopicGenerationController.addCourse
-);
-router.post("/generateTopics", TopicGenerationController.topicGeneration);
-router.post("/generateQuiz", TopicGenerationController.QuizGeneration )
-router.get("/getQuiz", generateQuiz.getQuiz )
+//Authentication
 router.use("/auth" ,authRoute);
 
+// router.get("/gets3", TopicGenerationController.topicGeneration);
+
+// Courses 
+router.get("/courses", AuthMiddleware, TopicGenerationController.getCourses);
+router.post("/addCourse", AuthMiddleware, upload.single("file"), TopicGenerationController.addCourse);
+router.post("/generateTopics", TopicGenerationController.topicGeneration);
+
+// Quizes
+router.get('/getQuizByUser', AuthMiddleware, QuizController.getQuizByUser)
+router.post("/generateQuiz", AuthMiddleware ,TopicGenerationController.QuizGeneration )
+router.get("/getQuiz", AuthMiddleware ,generateQuiz.getQuiz );
 router.post("/sendTopics", AuthMiddleware, QuizController.sendTopics);
 
 export default router;
